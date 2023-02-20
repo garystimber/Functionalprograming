31ce24150979c332bb1e67dce2e1e11d834aa4ed
garystimber <88552183+garystimber@users.noreply.github.com>
2023-02-20 19:29:15 +0600

    lab2
    lab2

---
+++ lab2.py
@@ -0,0 +1,10 @@
+def lab2():
+    firstname = "Adil"
+    lastname = "Zhanbek"
+    middlename = "Tursynbaiyli"
+
+    if firstname == "Adil":
+        print("That me")
+    else: print("That's not me")
+
+    #Commentary
